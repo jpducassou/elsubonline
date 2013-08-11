54f4f3b4b5f65dd103ab3e4590038ca72b6e48c0
Jean Pierre Ducassou <jpducassou@gmail.com>
2013-08-11 19:31:47 -0300

    Added email sending capability. WIP.

--- ejb/src/main/java/uy/com/elsubonline/messages/Registration.java
+++ ejb/src/main/java/uy/com/elsubonline/messages/Registration.java
@@ -1,9 +1,16 @@
 package uy.com.elsubonline.messages;
 
+import java.util.Properties;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import org.apache.log4j.Logger;
 
 @MessageDriven(
@@ -16,7 +23,43 @@ public class Registration implements MessageListener {
     @Override
     public void onMessage(Message message) {
         logger.info("Message arrived: " + message);
-        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // String notificationEmail = (TextMessage)message.get
+
+        final String username = "from@gmail.com";
+        final String password = "password";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+            new javax.mail.Authenticator() {
+                  @Override
+                  protected PasswordAuthentication getPasswordAuthentication() {
+                          return new PasswordAuthentication(username, password);
+                  }
+            }
+        );
+ 
+        try {
+
+            MimeMessage emailMessage = new MimeMessage(session);
+            emailMessage.setFrom(new InternetAddress("from@gmail.com"));
+            emailMessage.setRecipients(MimeMessage.RecipientType.TO,
+                    InternetAddress.parse("to@gmail.com"));
+            emailMessage.setSubject("Testing Subject");
+            emailMessage.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(emailMessage);
+
+            logger.info("Email sent to: " + message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
