c4fa046d32b19bed91dd6c4f08650d3ad360fb09
Jean Pierre Ducassou <jpducassou@gmail.com>
2013-08-15 17:40:01 -0300

    Fixed release related issues

---
+++ deployment/elsubonline.sql
@@ -0,0 +1,138 @@
+-- MySQL dump 10.13  Distrib 5.6.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: elsubonline
+-- ------------------------------------------------------
+-- Server version	5.6.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auctions`
+--
+
+DROP TABLE IF EXISTS `auctions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auctions` (
+  `id` bigint(20) NOT NULL,
+  `base_price` double NOT NULL,
+  `closing_time` datetime DEFAULT NULL,
+  `creation_time` datetime DEFAULT NULL,
+  `long_description` varchar(255) DEFAULT NULL,
+  `short_description` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `category_name` varchar(255) DEFAULT NULL,
+  `owner_email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK390EEA9067257BDB` (`category_name`),
+  KEY `FK390EEA9064A4D50C` (`owner_email`),
+  CONSTRAINT `FK390EEA9064A4D50C` FOREIGN KEY (`owner_email`) REFERENCES `users` (`email`),
+  CONSTRAINT `FK390EEA9067257BDB` FOREIGN KEY (`category_name`) REFERENCES `categories` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auctions`
+--
+
+LOCK TABLES `auctions` WRITE;
+/*!40000 ALTER TABLE `auctions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auctions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES ('Animales y Mascotas'),('Arte y Antigüedades'),('Computación'),('Deporte y Fitness'),('Electronica'),('Industrias y Oficinas');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (8);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `email` varchar(255) NOT NULL,
+  `administrator` tinyint(1) NOT NULL,
+  `creation_time` datetime DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `subscribed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('elsub.uy@gmail.com',1,'2013-08-15 15:46:51','Jean Pierre','Ducassou','elsubonline administrator','SHA1:7110eda4d09e062aa5e4a390b0a572ac0d2c0220','00000000','ACTIVE',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-08-15 17:27:02
--- readme.md
+++ readme.md
@@ -3,7 +3,17 @@ ElSubOnline
 
 Description
 -----------------------------
-JAVA EE auction web application
+JAVA EE auction web application.
+The source of this application is available on:
+* [bitbucket](https://bitbucket.org/jpducassou/elsubonline)
+* [github](https://github.com/jpducassou/elsubonline)
+
+See the "license" section below to know in which terms it is released.
+
+Licence
+----------------------------
+This software is released under GNU GENERAL PUBLIC LICENSE v2. See LICENSE.txt.
+Third party software bundled are covered by their respective licenses (i.e. mysql jdbc driver).
 
 Compilation
 -----------------------------
@@ -19,12 +29,17 @@ Alternatively you can put the generated ear file onto whichever folder you are s
 For more information visit [maven web site](http://maven.apache.org/).
 
 Notes:
-Jboss needs must be started with JMS support (i.e. -Djboss.server.default.config=standalone-full.xml)
+Jboss must be started with JMS support (i.e. -Djboss.server.default.config=standalone-full.xml)
 Also there is a directory called "deployments" with all you need to deploy this application:
 
-* mysql jdbc driver.
-* datasource configuration (elsubonline-ds.xml).
-* message queues configuration (elsubonline-hornetq-jms.xml).
+* The MySQL jdbc driver.
+* A datasource configuration file (elsubonline-ds.xml).
+* A message queue configuration file (elsubonline-hornetq-jms.xml).
+* A MySQL dump sample with some data as example. This includes an administrator user whose credentials are:
+	* username: elsub.uy@gmail.com
+	* password: 1234
+
+	Remember that to be able to load this dump you must create elsubonline database and grant access to a mysql user. (See elsubonline-ds.xml)
 
 Author
 ----------------------------
